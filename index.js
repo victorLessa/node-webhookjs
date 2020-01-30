1031610055c039378d58291f4f6ef1d54bc3e8e6
Camara <vlessa.terceiro@brasilcap.com.br>
2020-01-30 17:31:12 -0300

    refatoração teams e telegram

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const { teams } = require('./lib/teams')
-const { telegram } = require('./lib/telegram')
 
-module.exports = {
-  teams,
-  telegram
-}
+export const telegram = require('./lib/telegram')
+export const teams = require('./lib/teams')
+// module.exports = {
+//   ,
+// }
